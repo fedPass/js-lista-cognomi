4d8d87b81e1476181c8eb4ca8e3f6a1dad90543b
fedPass <federica.passanante@gmail.com>
2019-11-18 14:35:31 +0100

    index e main

---
+++ main.js
@@ -0,0 +1,12 @@
+// alert('prova');
+
+// stampa la lista ordinata alfabeticamente.
+// Scrivi anche la posizione della lista in cui  il nuovo utente si trova.
+
+var listaCognomi = ['Agrigento','Trapani','Palermo','Caltanissetta',];
+console.log(listaCognomi);
+// Chiedi all’utente il cognome
+var cognomeUtente = prompt('Inserisci il tuo cognome');
+console.log('Il cognome inserito è ' + cognomeUtente);
+
+// inseriscilo in un array con altri cognomi
